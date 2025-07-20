29f142cc1a195128545ebaa9d5aa9f825921fe55
congnghia0609 <congnghia0609@gmail.com>
2025-07-21 06:48:48 +0700

    Implement data persistence between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
@@ -16,5 +17,33 @@ public class MainManager : MonoBehaviour
     }
     Instance = this;
     DontDestroyOnLoad(gameObject);
+    LoadColor();
   }
+
+  // Ref: https://docs.unity3d.com/ScriptReference/Application-persistentDataPath.html
+  public void SaveColor()
+  {
+    SaveData data = new SaveData();
+    data.TeamColor = TeamColor;
+    string json = JsonUtility.ToJson(data);
+    File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+  }
+
+  public void LoadColor()
+  {
+    string path = Application.persistentDataPath + "/savefile.json";
+    Debug.Log(path);
+    if (File.Exists(path))
+    {
+      string json = File.ReadAllText(path);
+      SaveData data = JsonUtility.FromJson<SaveData>(json);
+      TeamColor = data.TeamColor;
+    }
+  }
+}
+
+[System.Serializable]
+public class SaveData
+{
+  public Color TeamColor;
 }
